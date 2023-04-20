a53982d177c524169aa6abd6604b789ca9d0f6dd
Wilson Wu <41039035+Wilson-Wu1@users.noreply.github.com>
2023-04-19 23:26:53 -0700

    Add files via upload

---
+++ test_code/StakingRewards/HelperRewardToken.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+contract RewardToken is ERC20{
+    constructor() ERC20("Gold", "GLD") public {
+        _mint(msg.sender, 100e8);
+    }
+}
