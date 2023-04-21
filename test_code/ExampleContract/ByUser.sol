0c34250939ec1cb53a4b3ad4446b17348bf00c17
Wilson Wu <41039035+Wilson-Wu1@users.noreply.github.com>
2023-04-20 21:46:36 -0700

    Add files via upload

---
+++ test_code/ExampleContract/ByUser.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract GlobalVariablePacking {
+
+    bytes16 var3;
+    uint128 var4;
+    uint128 var1;
+    uint128 var2;
+    bool var6;
+    uint88 var7;
+    address var5;
+    
+
+
+    function setVariables1and2() public {
+        var1 = 1;
+        var2 = 1;
+    }
+
+    function setVariables3and4() public {
+        var3 = "0";
+        var4 = 1;
+    }
+
+    function setVariables5and6and7() public {
+        var5 = msg.sender;
+        bool opposite = !var6;
+        var6 = opposite;
+        var7 = 1;
+    }
+
+
+
+
+}
